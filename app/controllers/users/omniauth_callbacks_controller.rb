5dde6386726565c19fd44bd5ce0d6049eca5bfc4
mariaaj03 <maj2198@columbia.edu>
2025-11-10 21:15:24 -0500

    Remove flash messages and fix export buttons layout

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         )
 
         sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
